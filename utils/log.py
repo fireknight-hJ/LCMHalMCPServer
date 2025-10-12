7cf359488aa4a79a904dcd4b7127e81141f12eb1
fireknight-hJ <1290208782@qq.com>
2025-10-12 18:14:54 +0800

    to: log support

---
+++ utils/log.py
@@ -0,0 +1,20 @@
+# 添加日志功能
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+
+
+# 定义日志函数，加入各种level
+def log_info(message: str):
+    logger.info(message)
+
+def log_error(message: str):
+    logger.error(message)
+
+def log_debug(message: str):
+    logger.debug(message)
+
+def log_warning(message: str):
+    logger.warning(message)
