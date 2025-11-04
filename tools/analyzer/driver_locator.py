c77a25e4da85bc67844690ccdb90df6e1f24f761
fireknight-hj-macbook <1290208782@qq.com>
2025-11-04 14:58:01 +0800

    fix: chat deepseek strut output

--- tools/analyzer/driver_locator.py
+++ tools/analyzer/driver_locator.py
@@ -2,7 +2,7 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.chat_models import init_chat_model
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import ToolNode
-from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek
 from langchain_core.messages import HumanMessage
 from config.llm_config import llm_deepseek_config
 from models.analyze_results.driverdir_locate import DriverDirLocatorResponse
@@ -13,10 +13,10 @@ from utils.db_cache import dump_message_json_log, check_analyzed_json_log
 import config.globs as globs
 
 # Initialize the model
-model = ChatOpenAI(
-    model=llm_deepseek_config["model_name"],
+model = ChatDeepSeek(
+    model=llm_deepseek_config["model_name"], 
     api_key=llm_deepseek_config["api_key"], 
-    base_url=llm_deepseek_config["base_url"]
+    api_base=llm_deepseek_config["base_url"]
 )
 
 # Set up MCP client
--- tools/builder/builder_tool.py
+++ tools/builder/builder_tool.py
@@ -2,7 +2,7 @@ from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.chat_models import init_chat_model
 from langgraph.graph import StateGraph, MessagesState, START, END
 from langgraph.prebuilt import ToolNode
-from langchain_openai import ChatOpenAI
+from langchain_deepseek import ChatDeepSeek
 from langchain_core.messages import HumanMessage
 from config.llm_config import llm_deepseek_config
 from models.build_results.build_output import BuildOutput
@@ -13,10 +13,10 @@ from utils.db_cache import dump_message_json_log, check_analyzed_json_log
 import config.globs as globs
 
 # Initialize the model
-model = ChatOpenAI(
-    model=llm_deepseek_config["model_name"],
+model = ChatDeepSeek(
+    model=llm_deepseek_config["model_name"], 
     api_key=llm_deepseek_config["api_key"], 
-    base_url=llm_deepseek_config["base_url"]
+    api_base=llm_deepseek_config["base_url"]
 )
 
 # Set up MCP client
@@ -72,7 +72,7 @@ class AgentState(MessagesState):
             tmp_dir = os.path.join(work_dir, "tmp")
             if not os.path.exists(tmp_dir):
                 os.makedirs(tmp_dir)
-            file_path = os.path.join(tmp_dir, f"driver_dir_locate_{time.strftime('%Y%m%d%H%M%S', time.localtime())}.json")
+            file_path = os.path.join(tmp_dir, f"builder_tool_{time.strftime('%Y%m%d%H%M%S', time.localtime())}.json")
         with open(file_path, "w", encoding="utf-8") as f:
             f.write(self.dump_message_json())
 
