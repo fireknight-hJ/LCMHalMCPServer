15cdcf8bba9b8f1352d3a0efa4db37caae761ed8
csejhin <u202211938@hust.edu.cn>
2026-01-24 02:52:15 +0000

    恢复Zephyr官方UART测试用例目录

---
+++ testcases/zephyr_uart_official/build/CMakeFiles/zephyr_uart_official.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zephyr_uart_official.dir/main.c.o"
+  "CMakeFiles/zephyr_uart_official.dir/main.c.o.d"
+  "zephyr_uart_official"
+  "zephyr_uart_official.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/zephyr_uart_official.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ testcases/zephyr_uart_official/build/CMakeFiles/zephyr_uart_official.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for zephyr_uart_official.
+# This may be replaced when dependencies are built.
