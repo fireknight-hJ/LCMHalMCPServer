95558a12525e941783879dfd6ec4e294e34f64d2
fireknight-hJ <1290208782@qq.com>
2026-02-16 19:22:03 +0800

    test: testcases更新server下nxp的demo

---
+++ testcases/server/nxp/NXP_LwIP_FreeRTOS/clear.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PWDDIR=/home/haojie/mcuxsdk-workspace
+
+if [ ! -d "$PWDDIR" ]; then
+    echo "错误: 构建目录不存在: $PWDDIR"
+    exit 1
+fi
+
+cd "$PWDDIR" || {
+    echo "错误: 无法切换到目录 $PWDDIR"
+    exit 1
+}
+
+echo "开始清除项目..."
+rm -rf build-eth-freertos
+
+if [ $? -eq 0 ]; then
+    echo "清除成功完成!"    
+else
+    echo "清除失败!"
+    exit 1
+fi
